887ffcf0828350480daf159346df119defd1d593
labdesarrollojava <lab.desarrollo.java@gmail.com>
2022-07-01 05:55:05 +0000

    Initial version of jhipster7 generated by generator-jhipster@7.8.1

---
+++ src/main/java/es/lab/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package es.lab.jhipster.repository;
+
+import es.lab.jhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
